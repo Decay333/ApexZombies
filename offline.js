e5dc6227b22e35a0db708d51a058b6148ca96037
Decay333 <142379712+Decay333@users.noreply.github.com>
2023-08-16 15:52:50 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1692175430,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/tiledbackground.png",
+		"images/bullet-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/Спрайт-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/ПлиточныйФон.png",
+		"images/ПлиточныйФон2.png",
+		"images/ПлиточныйФон3.png",
+		"media/5d1abc9d8609a59.ogg",
+		"media/a80ae40210cd350.ogg",
+		"media/dirty_deeds_done_dirt_cheap__instrumental_version_.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
